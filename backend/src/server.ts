ff400b8d3a43627a83023d7b4be8de82a93b652a
tawfiq2725 <tawfiqsiraj003003@gmail.com>
2025-05-07 22:52:42 +0530

    cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,21 +10,15 @@ dotenv.config();
 import authRoutes from "./routes/auth.routes";
 import adminRoutes from "./routes/admin.routes";
 import feedbackRoutes from "./routes/fb.route";
-import { frontendUrl } from "./config/auth.config";
 
 const app = express();
 const port = process.env.APP_PORT || 5000;
-const corsOptions = {
-  origin: frontendUrl() || "*",
-  methods: ["GET", "POST"],
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
+
 
 // application middlewares
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(morgan("dev"));
 // routes
 app.use("/api/fb", feedbackRoutes);
@@ -46,4 +40,4 @@ const serverStart = async () => {
   }
 };
 serverStart();
-frontendUrl();
+
